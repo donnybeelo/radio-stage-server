564efb95973057fce3a9d5f5a76d020f510d5edc
Dan <d.elia@outlook.com>
2025-02-20 09:32:47 +0000

    change HTTP handler to respond at /api path instead of /api/endpoints

--- server.go
+++ server.go
@@ -48,8 +48,8 @@ func generateEndpointPath() string {
 }
 
 func main() {
-	http.HandleFunc("/api/endpoints", handleEndpoints)
-	http.HandleFunc("/ws/", handleDynamicWebSocket)
+	http.HandleFunc("/api/", handleEndpoints)
+	http.HandleFunc("/api/ws/", handleDynamicWebSocket)
 
 	fmt.Println("Listening to port 8080")
 	http.ListenAndServe(":8080", nil)
